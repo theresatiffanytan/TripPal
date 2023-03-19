ad7d4195a4419fc0955d2648109ff977af8ca274
dekapd99 <dekaprimatio@gmail.com>
2023-03-19 13:57:54 +0700

    Initial Commit

---
+++ TripPal/TripPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPalApp.swift
+//  TripPal
+//
+//  Created by Deka Primatio on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TripPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
