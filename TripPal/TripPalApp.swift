259700917c2f8ff7ae998cc4bddb1b437ec45e07
Theresa Tiffany <theresatiffanytanadji@gmail.com>
2023-03-19 13:36:56 +0700

    Initial Commit

---
+++ TripPal/TripPalApp.swift
@@ -0,0 +1,20 @@
+//
+//  TripPalApp.swift
+//  TripPal
+//
+//  Created by Theresa Tiffany on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TripPalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
