98411a3e4d6ac79aa6f818abfea7c25748631309
dekapd99 <dekaprimatio@gmail.com>
2023-03-19 14:00:50 +0700

    first commit for detail page

---
+++ TripPal/DetailPageView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailPageView.swift
+//  TripPal
+//
+//  Created by Deka Primatio on 19/03/23.
+//
+
+import SwiftUI
+
+struct DetailPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPageView()
+    }
+}
